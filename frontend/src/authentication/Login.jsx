fc7bfcc2b5b48503630604affe7832d7d9d3e469
theh4cker1769 <reddy.har2002@gmail.com>
2023-08-22 13:24:10 +0530

    JWT Token Implemented | 22-08-2023

--- frontend/src/authentication/Login.jsx
+++ frontend/src/authentication/Login.jsx
@@ -4,26 +4,28 @@ import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         // Send username and password to the backend for verification
-        const response = fetch('/api/login', {
+        const response = await fetch('/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ username, password }),
         });
-        const data = response.json();
+        const data = await response.json();
+        console.log('data',data)
+        navigate('/home')
         // Store the JWT token in localStorage
         localStorage.setItem('token', data.token);
     }
 
     const loginEmailEvent = (e) => {
-        setEmail(e.target.value)
+        setUsername(e.target.value)
     }
     const loginPasswordEvent = (e) => {
         setPassword(e.target.value)
@@ -33,8 +35,8 @@ export const Login = (props) => {
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input onChange={loginEmailEvent} value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="email">Username</label>
+                <input onChange={loginEmailEvent} value={username} type="test" placeholder="username" id="email" name="email" />
 
                 <label htmlFor="password">Password</label>
                 <input onChange={loginPasswordEvent} value={password} type="password" placeholder="*******" id="password" name="password" />
